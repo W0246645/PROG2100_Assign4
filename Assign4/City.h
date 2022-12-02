9a64d288c257f654371e79822f287394a0d97e36
Matthew Covey <90411582+W0246645@users.noreply.github.com>
2022-12-02 15:40:52 -0400

    Got all of davids boiler plate headers in

---
+++ Assign4/City.h
@@ -0,0 +1,30 @@
+#ifndef _CITY_H
+#define _CITY_H
+
+#include <iostream>
+#include "GameSpecs.h"
+
+using namespace std;
+
+class Organism;
+
+class City
+{
+protected:
+	Organism *grid[GRIDSIZE][GRIDSIZE];
+
+public:
+	City();
+	virtual ~City();
+
+	Organism *getOrganism( int x, int y );
+	void setOrganism( Organism *organism, int x, int y );
+
+	void move();
+
+	friend ostream& operator<<( ostream &output, City &city );
+
+};
+
+#endif
+
